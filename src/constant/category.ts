656527c2d6ba535420c5f338512b441791fee517
rajeammu <raje30pa@gmail.com>
2025-05-05 17:09:37 +0530

    changes

--- src/constant/category.ts
+++ src/constant/category.ts
@@ -1,15 +1,14 @@
 import { CROPTOP, HOODIE, POLO, SWEATSHIRTS, TSHIRT } from "../assets";
 
 export const categoryData = [
-    { id: 1, name: 'polo', image: POLO },
-    { id: 2, name: 'hoodie', image: HOODIE },
-    { id: 3, name: 'tshirt', image: TSHIRT },
-    { id: 4, name: 'sweatshirts', image: SWEATSHIRTS },
-    { id: 5, name: 'croptop', image: CROPTOP },
-    { id: 6, name: 'polo', image: POLO },
-    { id: 7, name: 'hoodie', image: HOODIE },
-    { id: 8, name: 'tshirt', image: TSHIRT },
-    { id: 9, name: 'sweatshirts', image: SWEATSHIRTS },
-    { id: 10, name: 'croptop', image: CROPTOP }
+    { id: 1, name: 'Polo', image: POLO },
+    { id: 2, name: 'Hoodie', image: HOODIE },
+    { id: 3, name: 'T-shirt', image: TSHIRT },
+    { id: 4, name: 'Sweatshirts', image: SWEATSHIRTS },
+    { id: 5, name: 'Crop top', image: CROPTOP },
+    { id: 6, name: 'Tank top', image: POLO },
+    { id: 7, name: 'V-neck', image: HOODIE },
+    { id: 8, name: 'oversized', image: TSHIRT },
+   
   ];
   
--- src/constant/collection.ts
+++ src/constant/collection.ts
@@ -1,15 +1,15 @@
 import { KIDS, MENS, NEWCOLLECTION, WOMENS } from "../assets";
 
 export const Collections  = [
-    { id: 1, name: 'new collection', image: NEWCOLLECTION, categoryId: 1 },
-    { id: 2, name: 'mens', image: MENS, categoryId: 2 },
-    { id: 3, name: 'womens', image: WOMENS, categoryId: 3 },
-    { id: 4, name: 'kids', image: KIDS, categoryId: 1 },
-    { id: 5, name: 'Kids Fashion', image: NEWCOLLECTION, categoryId: 4 },
-    { id: 6, name: 'Couple Sets', image: MENS, categoryId: 2 },
-    { id: 7, name: 'Classic White', image: WOMENS, categoryId: 1 },
-    { id: 8, name: 'Hoodie Season', image: KIDS, categoryId: 5 },
-    { id: 9, name: 'new collection', image: NEWCOLLECTION, categoryId: 3 },
-    { id: 10, name: 'casual', image: MENS, categoryId: 0 }
+    { id: 1, name: 'new collection', image: NEWCOLLECTION },
+    { id: 2, name: 'mens', image: MENS },
+    { id: 3, name: 'womens', image: WOMENS},
+    { id: 4, name: 'kids', image: KIDS},
+    { id: 5, name: 'mens', image: NEWCOLLECTION },
+    { id: 6, name: 'kids', image: MENS},
+    { id: 7, name: 'womens', image: WOMENS },
+    { id: 8, name: 'mens', image: KIDS},
+    { id: 9, name: 'new collection', image: NEWCOLLECTION},
+    { id: 10, name: 'mens', image: MENS }
   ];
   
